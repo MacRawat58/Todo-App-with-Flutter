502420e720376003759ebe0962cb9a0d8c8ea8e6
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-06-12 14:14:58 +0530

    debugger status removed

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
