b3ed2f27037afa306e60d1c4ef71e99660bade65
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-06-12 14:06:35 +0530

    assigning Keys to elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_internals/ui_updates_demo.dart';
+import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
   runApp(const App());
@@ -17,7 +16,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        body: const Keys(),
       ),
     );
   }
--- lib/ui_updates_demo.dart
+++ lib/ui_updates_demo.dart
@@ -1,66 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internals/demo_buttons.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
-
-  @override
-  StatefulElement createElement() {
-    print('UIUpdatesDemo CREATEELEMENT called');
-    return super.createElement();
-  }
-
-  @override
-  State<UIUpdatesDemo> createState() {
-    return _UIUpdatesDemo();
-  }
-}
-
-class _UIUpdatesDemo extends State<UIUpdatesDemo> {
-  var _isUnderstood = false;
 
   @override
   Widget build(BuildContext context) {
     print('UIUpdatesDemo BUILD called');
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-            const Text(
+            SizedBox(height: 16),
+            Text(
               'Do you understand how Flutter updates UIs?',
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = false;
-                    });
-                  },
-                  child: const Text('No'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = true;
-                    });
-                  },
-                  child: const Text('Yes'),
-                ),
-              ],
-            ),
-            if (_isUnderstood) const Text('Awesome!'),
+            SizedBox(height: 24),
+            DemoButtons(),
           ],
         ),
       ),
